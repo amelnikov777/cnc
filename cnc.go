44a78daa32f4c77d272dff4c423deb6af4e1c1d1
Andrei Melnikov <125220410+amelnikov777@users.noreply.github.com>
2023-11-25 21:37:57 +0200

    ver 0.4
    Import format ScatchCut has done.

--- cnc.go
+++ cnc.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -11,9 +12,11 @@ import (
 var array []string
 var Header map[string]string
 var Details map[string]map[string]int
+var DetailIndex int
 
 func main() {
 	Header = make(map[string]string)
+	Details = make(map[string]map[string]int)
 	ReadFile()
 	MakeHeader([]string{"PlateLenght", "PlateWidth", "PlatesQty"}, 2)
 	MakeHeader([]string{"CutNum?", "CutThickness", "EdgeMargin", "CutBol?"}, 3)
@@ -27,16 +30,22 @@ func main() {
 	} else {
 		Header["DetailStartIndex"] = "16"
 	}
-
-	// Details = make(map[string]map[string]int)
-
-	Details = map[string]map[string]int{
-		"деталь1": {"длина": 1, "ширина": 2},
-		"деталь2": {"длина": 3, "ширина": 4},
-	}
-	fmt.Println(Details)
+	DetailStopIndex()
 
 	printarray(Header)
+
+	DetailStartIndex, _ := strconv.Atoi(Header["DetailStartIndex"])
+	DetailStopIndex, _ := strconv.Atoi(Header["DetailStopIndex"])
+	// for i := DetailStartIndex; i <= DetailStopIndex; i++ {
+	// 	fmt.Println("details ", i, " ", array[i])
+	// }
+	DetailIndex = DetailStartIndex
+
+	for DetailIndex < DetailStopIndex {
+		MakeDetail(DetailIndex)
+
+	}
+
 	printdetails(Details)
 }
 
@@ -67,10 +76,11 @@ func ReadFile() { //This function reads user sct file and writes it to array.
 	}
 	fmt.Println("Прочитано строк: ", len(array))
 
-	for i, arrayslice := range array {
-		fmt.Println(i, " ", arrayslice)
-	}
+	// 	for i, arrayslice := range array {
+	// 		fmt.Println(i, " ", arrayslice)
+	// 	}
 }
+
 func printarray(array map[string]string) {
 	for i, ii := range array {
 		iiint, iierr := strconv.Atoi(ii)
@@ -82,9 +92,69 @@ func printarray(array map[string]string) {
 	}
 }
 func printdetails(array map[string]map[string]int) {
+	keys := make([]string, 0, len(array))
+	for k := range array {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	for _, i := range keys {
+		fmt.Println(i)
+		fmt.Print("PlateNumber ", array[i]["PlateNumber"], " DetailLenght ", array[i]["DetailLenght"], " DetailWidth ", array[i]["DetailWidth"], " X ", array[i]["X"], " Y ", array[i]["Y"])
+		fmt.Println()
+	}
+}
+
+// This function adds DetailFinishIndex line number to Header
+func DetailStopIndex() {
 	for i, ii := range array {
-		for j, jj := range ii {
-			fmt.Println(i, " ", j, " ", jj)
+		if strings.Contains(ii, "<USnips>") {
+			Header["DetailStopIndex"] = strconv.Itoa(i - 1)
 		}
 	}
+}
+
+func MakeDetail(Index int) {
+	DetailNam := array[Index+2]
+	DetailName := DetailNam[2:]
+	DetailList := DetailsConvert(Index)
+	// Details = map[string]map[string]int{
+	// 	DetailName: {"DetailLenght": DetailList[0], "DetailWidth": DetailList[1], "DetailQty": DetailList[2]},
+	// }
+	DetailIndex += 3
+	DetailNumber := 0
+	DetailScanStop := DetailIndex + DetailList[2]
+	for DetailIndex < DetailScanStop {
+		DetailNumber++
+		DetailProperty := DetailsConvert(DetailIndex)
+		Details[DetailName+" "+strconv.Itoa(DetailList[0])+" "+strconv.Itoa(DetailList[1])+" "+strconv.Itoa(DetailNumber)] = map[string]int{
+
+			"DetailLenght": DetailList[0],
+			"DetailWidth":  DetailList[1],
+			"DetailQty":    DetailList[2],
+			"DetailNumber": DetailNumber,
+			"PlateNumber":  DetailProperty[0] % 10,
+			"X":            DetailProperty[2],
+			"Y":            DetailProperty[3],
+		}
+		DetailIndex++
+	}
+
+}
+
+func DetailsConvert(line int) []int {
+	DetailString := make([]string, 4)
+	DetailStringInt := make([]int, 4)
+	key := 0
+	for _, ii := range array[line] {
+		if string(ii) != "X" && string(ii) != "_" && string(ii) != ">" {
+			DetailString[key] += string(ii)
+		} else {
+			key++
+		}
+	}
+	DetailStringInt[0], _ = strconv.Atoi(DetailString[0])
+	DetailStringInt[1], _ = strconv.Atoi(DetailString[1])
+	DetailStringInt[2], _ = strconv.Atoi(DetailString[2])
+	DetailStringInt[3], _ = strconv.Atoi(DetailString[3])
+	return DetailStringInt
 }
